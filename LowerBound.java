6c8a8fba4638b22d4aeb55c6ec260f7f6e494339
Ervin Kalemi <ervin_kalemi@yahoo.com>
2011-09-21 11:34:12 -0500

    Updated info and added assertion for input file

--- LowerBound.java
+++ LowerBound.java
@@ -4,13 +4,17 @@ Ervin Kalemi: Slip days used for this project: 0  Slip days used (total): 0
 
 Pair programming log (> 80% paired)
 9/19 3-4a  Ian, 1 hr
-9/20 8:30-10p Ian Ervin, 4 hrs
+9/20 7-12p Ian, Ervin, 5 hrs
 
-Total time X hrs, Z hrs of pair programing
+Total time 6 hrs, 5 hrs of pair programing
 
-Challanges:
+Challenges: Trying to calculate the entropy when the number of occurence of
+one of the characters was zero. It returned NaN. To fix this, we checked if the occurence
+of the character wasn't zero we running the entropy formula.
 
-Learned:
+Learned: To round a number to certain decimals, you simply do 
+round that number * 10^(#decimals), and divide that number by
+10^(#decimals).
 
 Notes:
 run with commands:
@@ -21,26 +25,16 @@ java -ea LowerBound smaller.txt
 
 //Ian driving now
 //swap every ~30 min
-
 public class LowerBound {
-  
-	
 	static final int ASCII = 255;
-  public static void main(String[] args) throws Exception {
+    
+    public static void main(String[] args) throws Exception {
   	//assertion
-  	boolean youRead = true;
-  	assert(youRead == true);
-  	
-  	
-  	
-  	//testing args[]
-  	String file = null;
-  	
-  	if (args.length > 0)
-  	  file = args[0];
+  	assert(args.length == 1);
+    // Input file
+    String file = args[0];
   	
   	//IO object
-  	//try exception
   	IO.Compressor compressor = new IO.Compressor(file);
   	char[] charArray = compressor.getCharacters();
   	
@@ -49,7 +43,7 @@ public class LowerBound {
   	//Ervin driving now
   	for(int i = 0; i < charArray.length; i++)
   		occurence[ (int)charArray[i] ]++;
-  	
+  	// Calculate entropy and lower bound
   	double e =  entropy(occurence, (double) charArray.length);  	
   	double lowBound =  lowerBound(e, charArray.length);
   	
@@ -59,16 +53,19 @@ public class LowerBound {
   
   static double entropy(int[] occurence, double size){
     double e = 0.0;
+    // For every occurence, if any, calculate run the entropy formula
     for (int i=0; i<occurence.length; i++){
     	if(occurence[i] != 0){
     		e -= (occurence[i]/size) * (Math.log(occurence[i]/size)/Math.log(2));
     	}
     }
+    // Round the entropy to three decimals and return it
     e = Math.round(e * 1000);
   	return e / 1000;
   }
   
   static double lowerBound(double e, int size ){
+    // Round the lower bound to three decimals and return it
   	double lowerBound = Math.round(size * e * 1000);
   	return lowerBound/1000;
   }
